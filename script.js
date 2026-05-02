b6f8b42a974ff91670ff849a99ef9a348055a6b3
THARVESH MUHAIDEEN A <tharvesh2026@gmail.com>
2026-05-02 10:26:34 +0530

    Add landing page files to SaaS project repository.

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.cta-button').addEventListener('click', function() {
+    alert('Signing up!');
+});
+
+window.addEventListener('scroll', function() {
+    // Smooth scrolling behaviour
+    let scrollPos = document.documentElement.scrollTop;
+});
+
+// Mobile menu toggle functionality (to be implemented)
+// Form handling and interactive elements (to be implemented)
