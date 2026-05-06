966b8166300f983465d8f56f988daf6a77389b8e
imtharvesh <imtharvesh@gmail.com>
2026-05-06 04:49:13 +0000

    Feat: Student Data Jar

---
+++ com/example/model/Department.java
@@ -0,0 +1,14 @@
+package com.example.model;
+
+public class Department {
+    private String deptName;
+
+    public Department(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public void showDept() {
+        System.out.println("Department: " + deptName);
+    }
+}
+
---
+++ com/example/model/Student.java
@@ -0,0 +1,15 @@
+package com.example.model;
+
+public class Student {
+    private String name;
+    private int id;
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void display() {
+        System.out.println("Student: " + name + ", ID: " + id);
+    }
+}
